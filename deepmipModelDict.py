fb7e0ed10e3e49a28cae031de39d4e2eea8317b1
Sebastian Steinig <seb.steinig@gmail.com>
2021-07-05 20:27:37 +0100

    added paleogeographic plot

--- deepmipModelDict.py
+++ deepmipModelDict.py
@@ -1,50 +1,50 @@
 #!/usr/bin/env python
 import numpy as np
 
-deepmipDict    = dict()
-deepmipDict['CESM1.2_CAM5']    = { \
+deepmipModelDict    = dict()
+deepmipModelDict['CESM1.2_CAM5']    = { \
         'versn' : 'v1.0',
         'group' : 'CESM',
         'pcolor': 'red',
         }
 
-deepmipDict['COSMOS-landveg_r2413']  = { \
+deepmipModelDict['COSMOS-landveg_r2413']  = { \
         'versn' : 'v1.0',
         'group' : 'COSMOS',
         'pcolor': 'magenta',
         }
 
-deepmipDict['GFDL_CM2.1']      = { \
+deepmipModelDict['GFDL_CM2.1']      = { \
         'versn' : 'v1.0',
         'group' : 'GFDL',
         'pcolor': 'orange',
         }
 
-deepmipDict['HadCM3B_M2.1aN']  = { \
+deepmipModelDict['HadCM3B_M2.1aN']  = { \
         'versn' : 'v1.0',
         'group' : 'HadCM3',
         'pcolor': 'maroon',
         }
 
-deepmipDict['HadCM3BL_M2.1aN'] = { \
+deepmipModelDict['HadCM3BL_M2.1aN'] = { \
         'versn' : 'v1.0',
         'group' : 'HadCM3',
         'pcolor': 'darkgoldenrod',
         }
 
-deepmipDict['INM-CM4-8']    = { \
+deepmipModelDict['INM-CM4-8']    = { \
         'versn' : 'v1.0',
         'group' : 'INMCM',
         'pcolor': 'lime',
         }
 
-deepmipDict['IPSLCM5A2']    = { \
+deepmipModelDict['IPSLCM5A2']    = { \
         'versn' : 'v1.0',
         'group' : 'IPSL',
         'pcolor': 'blue',
         }
 
-deepmipDict['MIROC4m']   = { \
+deepmipModelDict['MIROC4m']   = { \
         'versn' : 'v1.0',
         'group' : 'MIROC',
         'pcolor': 'darkgreen',
---
+++ deepmipVariableDict.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import numpy as np
+
+deepmipVariableDict    = dict()
+deepmipVariableDict['tas']    = { \
+        'longname' : 'near-surface air temperature',
+        'label' : 'temperature [$^\circ$C]',
+        }
+
+deepmipVariableDict['pr']    = { \
+        'longname' : 'precipitation',
+        'label' : 'total precipitation [mm/day]',
+        }
+        
+deepmipVariableDict['tos']    = { \
+        'longname' : 'sea surface temperature',
+        'label' : 'temperature [$^\circ$C]',
+        }
