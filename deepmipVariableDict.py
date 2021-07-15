1cb84e1a65bbda723d75e99582e322917499c301
Sebastian Steinig <seb.steinig@gmail.com>
2021-07-15 12:39:38 +0100

    three panels finished

--- deepmipVariableDict.py
+++ deepmipVariableDict.py
@@ -5,14 +5,17 @@ deepmipVariableDict    = dict()
 deepmipVariableDict['tas']    = { \
         'longname' : 'near-surface air temperature',
         'label' : 'temperature [$^\circ$C]',
+        'unit' : 'degC',
         }
 
 deepmipVariableDict['pr']    = { \
         'longname' : 'precipitation',
         'label' : 'total precipitation [mm/day]',
+        'unit' : 'mm/day',
         }
         
 deepmipVariableDict['tos']    = { \
         'longname' : 'sea surface temperature',
         'label' : 'temperature [$^\circ$C]',
+        'unit' : 'degC',
         }
